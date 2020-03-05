557e6bab2eef1340c4e9299f92dd98675a861cc9
mzloria <mikeeloria@gmail.com>
2020-03-05 14:53:59 -0800

    Added ken codex2

--- kgmr.js
+++ kgmr.js
@@ -138,15 +138,15 @@ KGMR.prototype.selectAction = function () {
         tempDir = direction(leader, this);
     }
 
-    if(this.x <= 10 && tempDir.x < 0 && this.x >= 790 && tempDir.x > 0) {
-        tempDir.x = 0;
-    } else if (this.y <= 10 && tempDir.y < 0 && this.y >= 790 && tempDir.y > 0) {
-        tempDir.y = 0;
-    }
 	tempDir.x += tempDir.x * 10000;
 	tempDir.y += tempDir.y * 10000;
 	// tempDir.x -= this.velocity.x;
-	// tempDir.y -= this.velocity.y;
+    // tempDir.y -= this.velocity.y;
+    if (this.x <= 10 && tempDir.x < 0 || this.x >= 790 & tempDir.x > 0) {
+		tempDir.x = 0;
+	} else if (this.y <= 10 & tempDir.y < 0 || this.y >= 790 & tempDir.y > 0) {
+		tempDir.y = 0;
+	}
 	action.direction = tempDir;
 
 	if (closestZ < 140) {
